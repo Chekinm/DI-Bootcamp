5d097d7832e22548aef0c74255d991b7b1082595
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-03-22 00:42:32 +0300

    fix

--- Week-1/Day-3/Daily-ninja-challenge/generator.py
+++ Week-1/Day-3/Daily-ninja-challenge/generator.py
@@ -1,2 +1,2 @@
 for i in range(256):
-    print('<p class=\"col' + str(i % 4 + 1) + ' block\"></p>')
+    print('<p class=\"col' + str(i % 3 + 1) + ' block\"></p>')
---
+++ Week-1/Day-3/Daily-ninja-challenge/generator1.py
@@ -0,0 +1,2 @@
+for i in range(256):
+    print('<p class=\"col' + str(i % 4 + 1) + ' block\"></p>')
