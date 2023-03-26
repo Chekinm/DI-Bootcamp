c3b385c3506a0d6e13be7090bff3f5e421cd98ee
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-03-26 14:15:48 +0300

    roll back as it add spare spaces to the string

--- Week-2/Day-1/XP_ninja/ninja.py
+++ Week-2/Day-1/XP_ninja/ninja.py
@@ -2,15 +2,7 @@
 #the following text, use a single line of code 
 #(beyond the establishment of your my_text variable).
 
-my_text = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore 
-        magna aliqua. Ut enim ad minim veniam, quis nostrud 
-        exercitation ullamco laboris nisi ut aliquip ex ea 
-        commodo consequat. Duis aute irure dolor in reprehenderit 
-        in voluptate velit esse cillum dolore eu fugiat nulla 
-        pariatur. Excepteur sint occaecat cupidatat non proident, 
-        sunt in culpa qui officia deserunt mollit anim id est 
-        laborum.'''
+my_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 print(f'My_Text var has {len(my_text)} character including spaces, dots and commas.')
 
@@ -23,13 +15,12 @@ num_of_words = len(my_text.replace(',','').replace('.','').split())
 print(f'And it has {num_of_words} words!')
 print('--------------------------------------------------')
 
-# Keep asking the user to input the longest sentence 
-# they can without the character “A”.
-# Each time a user successfully sets a new longest sentence, 
-# print a congratulations message.
+# Keep asking the user to input the longest sentence they can without the character “A”.
+# Each time a user successfully sets a new longest sentence, print a congratulations message.
 
 print('Now we will play a game!')
 print('Each time you enter a sentences, I check if it has an \'a\' simbol inside')
+      
 print('if know, i check the lenth of the phrase, and if it is longer then previuse one')
 print('you have one more step! if shorter, your game is over!')
 print('Let\'s go!')
