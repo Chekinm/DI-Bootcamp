7abf879a1b27c97124051412111640ccd2a466fa
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-03-28 15:25:16 +0300

    exercise Xp finished

--- Week-2/Day-3/ExerciseXP/cinema.py
+++ Week-2/Day-3/ExerciseXP/cinema.py
@@ -1,84 +1,107 @@
-###########################################################################################
-################   ex9 - ##################################################################
-###########################################################################################
-# Instructions
-# A movie theater charges different ticket prices depending on a person’s age.
-# if a person is under the age of 3, the ticket is free.
-# if they are between 3 and 12, the ticket is $10.
-# if they are over the age of 12, the ticket is $15.
+# cinema library
+# implemented classes:
 
-# Ask a family the age of each person who wants a ticket.
-
-# Store the total cost of all the family’s tickets and print it out.
-
-# A group of teenagers are coming to your movie theater and want to watch a movie that is restricted for people between the ages of 16 and 21.
-# Given a list of names, write a program that asks teenager for their age, if they are not permitted to watch the movie, remove them from the list.
-# At the end, print the final list.
-
-# I combine the rules of both problem into one.
-# You have been asked to enter number of people in the group
-# then name and age of each memeber of the group
-# then the type of file they want to watch
-# programm will print the verdict depending on members age ages
 
 class watcher():
-    def __init__(self, name, age, restricted="False", price=15):
+    """ hold information about person, constructor calculate
+        age price group according to bondles, witch can be ajusted
+        there is also restricted attributes for adults restritions"""
+    def __init__(self, name, age, restricted="False"):
         kid_age = 3
-        kid_price = 0
         teen_age = 12
-        teen_price = 10
         adult_age = 21
-        adult_price = 15
-        
         self.name = name
         self.age = age
+        self.price_group = '' 
         self.restricted = (True if age < adult_age else False) 
         # I change this from original to be logical -> just less than 21
         if self.age < kid_age:
-            self.price = kid_price
+            self.price_group = 'kid'
         elif self.age < teen_age:
-            self.price = teen_price
+            self.price_group = 'teen'
         else:
-            self.price = adult_price
+            self.price_group = 'adult'
 
-class watchers(list):
-    def __init__(self, watchers_list=None, total_price = 0):
-        if watchers_list == None:
+class watchers():
+    """holds groups of watchers as list of class wather object 
+        there is two method to fill the group
+        - read from dict ({name:age})
+        - read from input
+    """
+    def __init__(self, watchers_dict=None):
+        if watchers_dict == None:
             self.watchers_list = []
         else:
-            self.watchers_list = self.watchers_list 
+            self.watchers_list = []
+            for name, age in watchers_dict.items():
+                self.watchers_list.append(watcher(name,age))
     
     def read_dict(self, dict_of_watchers):
         for name, age in dict_of_watchers.items():
             self.watchers_list.append(watcher(name,age))
 
-
+    def clear(self):
+        self.watchers_list.clear()
+            
     def read_input(self):
         print('Enter number of persons in the group:')
         num_of_person = int(input())
+        self.watchers_list = []
         for i in range(num_of_person):
             print(f'Enter the name of the person number {i+1}:')
             name_i = input()
             print(f'Enter the age of the person number {i+1}:')
             age_i = int(input())
-            self.watchers_list.append(watcher(name_i,age_i))
+            self.watchers_list.append(watcher(name_i, age_i))
+        print(self.watchers_list[0].name)
 
         
-    def print_ticket(self, is_film_adult=False):
+class movie():
+    """
+    bocie class to hold name and age restriction
+    """
+    def __init__(self, name, is_adult=False):
+        self.name = name
+        self.is_adult = is_adult
+    
+
+class cinema():
+    """
+    cinema class holds the price list and has a method to issue 
+      a ticket to a group of wathers, depends on movie and ages of the group
+    """
+
+
+    def __init__(self, price_list=None):
+        if price_list == None:
+            self.price_list = {
+                'kid': 0,
+                'teen': 10,
+                'adult': 15,
+            }
+        else:
+            self.price_list = price_list
+            
+
+    def print_ticket(self, watchers_list, movie):
+        """ Gets wathers list (as watchers) and movie as movie and 
+        print a ticket for wathers list
+        """
         self.total = 0
         forbiden_list = []
         allowed_list = []
         print('-------------------------------------') 
-        for person in self.watchers_list:
-            if is_film_adult and person.restricted:
+        for person in watchers_list:
+            if movie.is_adult and person.restricted:
                 forbiden_list.append(person.name + ' - ' + str(person.age) + ' years')
                 continue
-            self.total += person.price
-            all_per_str = f'{person.name} \t  --- {person.price} shekels'
-            allowed_list.append(all_per_str)
+            person_price = self.price_list[person.price_group]
+            self.total += person_price
+            allowed_per_str = f'{person.name} \t  --- {person_price} shekels'
+            allowed_list.append(allowed_per_str)
             
         if forbiden_list:
-            print('Unfortunatley this person is not allowed to watch the movie:')
+            print(f'Unfortunatley this person is not allowed to watch the {movie.name}:')
             print(*forbiden_list, sep='\n')
             if allowed_list:
                 print('This is a check for others:')
@@ -87,8 +110,10 @@ class watchers(list):
                 print("Here is you check:")
                     
         if allowed_list:
+            print(f'You bought a group ticket to {movie.name}:')
             print(*allowed_list, sep='\n')
             print(f'total price is   {self.total}  shekels')
         
         print('-------------------------------------') 
+
 
--- Week-2/Day-3/ExerciseXP/exercise_xp.py
+++ Week-2/Day-3/ExerciseXP/exercise_xp.py
@@ -14,11 +14,11 @@ assert res_dict == {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 
 
 ###########################################################################################
-################   ex1   ##################################################################
+################   ex2   ##################################################################
 ###########################################################################################
 
 
-# Exercise 2 : Cinemax
+# it is in separate fileExercise 2 : Cinema
 # Instructions
 # A movie theater charges different ticket prices depending on a person’s age.
 # if a person is under the age of 3, the ticket is free.
@@ -41,17 +41,144 @@ assert res_dict == {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 # abusing DRI principle. Lets use class and functions writen yesterday.
 # i organize it a separate module cinema.py 
 
-from cinema import watcher, watchers
+from cinema import watchers, movie, cinema
 
-family1 = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+family1 = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 28}
 
-family_1_watchers = watchers()
-family_1_watchers.read_dict(family1)
-family_1_watchers.print_ticket()
+family_1_watchers = watchers(family1)
 
-# family2 = {}
+my_cinema = cinema({'kid':2,'teen':23, 'adult':100})
+
+my_movie = movie('Kill Bill', True)
+
+my_cinema.print_ticket(family_1_watchers.watchers_list, my_movie)
+
+family2 = {}
 
 family_2_watchers = watchers()
 family_2_watchers.read_input()
-family_2_watchers.print_ticket()
+my_movie2 = movie('Blue moon', True)
+my_cinema2 = cinema()
+my_cinema2.print_ticket(family_2_watchers.watchers_list, my_movie2)
+
+
+###########################################################################################
+################   ex3   ##################################################################
+###########################################################################################
+
+
+# 2. Create a dictionary called brand which value is the information from part one (turn the info into keys and values).
+
+brand_dict = {
+                'name':'Zara', 
+                'creation_date':1975,
+                'creator_name':'Amancio Ortega Gaona',
+                'type_of_clothes':['men', 'women', 'children', 'home'],
+                'international_competitors':['Gap', 'H&M', 'Benetton'],
+                'number_stores':7000,
+                'major_color':{
+                    'France':'blue',
+                    'Spain':'red', 
+                    'US':['pink', 'green'],
+                    },
+            }
+
+# 3. Change the number of stores to 2.
+brand_dict['number_stores'] = 2
+
+# 4. Print a sentence that explains who Zaras clients are.
+print('The customers of zara are: {}'.format(','.join(brand_dict['type_of_clothes'])))
+
+# 5. Add a key called country_creation with a value of Spain.
+brand_dict['country_creation']='Spain'
+# 6. Check if the key international_competitors is in the dictionary. If it is, add the store Desigual.
+if brand_dict.get('international_competitors') != None:
+    brand_dict.get('international_competitors').append('Desigual')
+print(brand_dict['international_competitors'])
+
+# 7. Delete the information about the date of creation.
+
+brand_dict.pop('creation_date')
+print(brand_dict)
+# 8. Print the last international competitor.
+print(brand_dict['international_competitors'][-1])
+
+# 9. Print the major clothes colors in the US.
+print(brand_dict['major_color']['US'])
+# 10. Print the amount of key value pairs (ie. length of the dictionary).
+print(len(brand_dict))
+
+# 11. Print the keys of the dictionary.
+print(brand_dict.keys())
+# 12. Create another dictionary called more_on_zara with the following details:
+more_on_zara = {
+    'creation_date': 1975,
+    'number_stores': 10000,
+    }
+
+# 13. Use a method to add the information from the dictionary more_on_zara to the dictionary brand.
+brand_dict.update(more_on_zara)
+print(brand_dict)
+
+
+# 14. Print the value of the key number_stores. What just happened ?
+print(brand_dict['number_stores'])
+
+###########################################################################################
+################   ex4   ##################################################################
+###########################################################################################
+# Exercise 4 : Disney Characters
+
+
+users = ["Mickey", "Minnie", "Donald", "Ariel", "Pluto"]
+
+
+#1
+disney_users_A={}
+disney_users_B={}
+
+for i, value in enumerate(users):
+    disney_users_A[value] = i
+    disney_users_B[i] = value
+
+print(disney_users_A)
+# {"Mickey": 0, "Minnie": 1, "Donald": 2, "Ariel": 3, "Pluto": 4}
+
+# 2
+print(disney_users_B)
+# {0: "Mickey",1: "Minnie", 2: "Donald", 3: "Ariel", 4: "Pluto"}
+
+# #3/ 
+disney_users_C={}
+for i, value in enumerate(sorted(users)):
+    disney_users_C[value] = i
+   
+print(disney_users_C)
+# {"Ariel": 0, "Donald": 1, "Mickey": 2, "Minnie": 3, "Pluto": 4}
+
+
+# Use a for loop to recreate the 1st result. Tip : don’t hardcode the numbers.
+# Use a for loop to recreate the 2nd result. Tip : don’t hardcode the numbers.
+# Use a method to recreate the 3rd result. Hint: The 3rd result is sorted alphabetically.
+# Only recreate the 1st result for:
+# The characters, which names contain the letter “i”.
+
+disney_users_A1={}
+
+for i, value in enumerate(users):
+    if 'i' in value:
+        disney_users_A1[value] = i    
+print(disney_users_A1)
+
+# The characters, which names start with the letter “m” or “p”.
+
+disney_users_A2={}
+
+
+for i, value in enumerate(users):
+    if  value[0].lower() in ['m','p']:
+        disney_users_A2[value] = i
+    
+print(disney_users_A2)
+
 
