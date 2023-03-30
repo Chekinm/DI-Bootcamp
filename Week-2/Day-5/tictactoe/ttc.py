a0e8fb0c04bfc044410b315387cb58353f430d46
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-03-30 14:11:37 +0300

    you can play

---
+++ Week-2/Day-5/tictactoe/ttc.py
@@ -0,0 +1,110 @@
+# display_board() – To display the Tic Tac Toe board (GUI).
+# player_input(player) – To get the position from the player.
+# check_win() – To check whether there is a winner or not.
+# play() – The main function, which calls all the functions created above.
+import numpy as np
+import os
+
+
+
+def display_board(game_map):
+    """game_map is a 3x3 array with game state
+    just print it put in fancy table""" 
+    
+    print('*' * 13)
+    for i in range(3):
+        print(f'* {game_map[i][0]} | {game_map[i][1]} | {game_map[i][2]} *')
+        if i < 2:
+            print('*---|---|---*')
+        else:
+            print('*' * 13)
+
+
+def check_win(game_map):
+    """return -1 if x win 0  is nobody and  1 if o win
+    """
+    game_matrix = np.zeros((3, 3))
+    for i in range(3):
+        for j in range(3):
+            if game_map[i][j] == x:
+                game_matrix[i][j] = 1
+            elif game_map[i][j] == o:
+                game_matrix[i][j] = -1
+    for i in range(2):
+        for sum_line in np.sum(game_matrix,i):
+            if sum_line == 3:
+                return 1
+            elif sum_line == -3:
+                return -1
+    if np.trace(game_matrix) == 3:
+        return 1
+    elif np.trace(game_matrix) == -3:
+        return -1
+    if np.trace(np.fliplr(game_matrix)) == 3:
+        return 1
+    elif np.trace(np.fliplr(game_matrix)) == -3:
+        return -1
+    return 0
+
+def player_input(player, game_map):
+    print(f'Now {player} turn.')
+    while True:
+        x_coor = int(input('Enter row: '))
+        y_coor = int(input('Enter column: '))
+        if game_map[x_coor-1][y_coor-1] == ' ':
+            game_map[x_coor-1][y_coor-1] = player
+            os.system('cls')
+            display_board(game_map)
+            return True
+        print('Sorry this field is occupaed. Enter another field')
+
+def game_clear(game_map):
+    for i in range(3):
+        for j in range(3):
+            game_map[i][j] = ' '
+
+def play():
+    game_map = [[' ' for i in range(3)] for j in range(3)]
+    counter = 0
+    while counter < 9 and check_win(game_map) == 0:
+        player_input((x if counter%2 else o), game_map)
+        counter += 1
+    else:
+        os.system('cls')
+        print('***********************')
+        print('******Game over!*******')
+        check = check_win(game_map)
+        if check == 1:
+            winner = x
+        elif check == -1:
+            winner = o
+        else:
+            winner = False
+        if winner:
+            print(f'*******\"{winner}\" won!********')
+            print('***********************')
+                   
+        else:
+            print('*****Nobody wins*******')
+
+
+
+    display_board(game_map)
+    
+
+
+
+
+#Fedya's staff
+x = "\033[91m{}\033[00m".format("x")
+o = "\033[93m{}\033[00m".format('o')
+
+# game_map=[
+#     [o,x,x],
+#     [' ',o,' '],
+#     [' ',' ',o]
+# ]  
+
+play()
+
+
