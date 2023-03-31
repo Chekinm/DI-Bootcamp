f5ef837b68e9cf58780b64a2072aa16574b81b6d
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-04-01 00:04:19 +0300

    temp

--- Week-2/Day-5/tictactoe/tmp1.py
+++ Week-2/Day-5/tictactoe/tmp1.py
@@ -1,3 +1,9 @@
-import numpy as np
-a = np.matrix([[1,2],[2,1]])
-print(a.diagonal(axis1=0, axis2=1))
+from functools import reduce
+from operator import mul
+def fact(n):
+    return reduce(mul, iter(range(1,n)))
+
+print(fact(10))
+
+
+
