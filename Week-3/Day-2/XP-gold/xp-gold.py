113d00277b3c1cf54857906d4e2f00c55b72996c
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-04-06 00:38:31 +0300

    xp-gold

--- Week-3/Day-2/XP-gold/xp-gold.py
+++ Week-3/Day-2/XP-gold/xp-gold.py
@@ -1,14 +1,17 @@
 # Exercise 1: Bank Account
 # Part I:
 
+import time
+
 class BankAccount():
     
-
+    
     def __init__(self, username, password, balance = 0, authenticated = False) -> None:
         self.balance = balance
         self.username = username
         self.password = password
         self.authenticated  = authenticated 
+
 
     def authenticate (self):
         username = input('Enter user name: ')
@@ -17,6 +20,14 @@ class BankAccount():
             self.authenticated = True
         else:
             print('User name does not exist or password is incorrect')
+
+
+    def authenticate_api (self, username, password):
+        if username == self.username and password == self.password:
+            self.authenticated = True
+            return True
+        else:
+            return False
 
 
     def deposit (self, amount):
@@ -36,30 +47,6 @@ class BankAccount():
         else:
             raise ConnectionError ('You are not logged it. PLease login first')
 
-
-a = BankAccount( '1', '1', 100,)
-print(a.balance)
-a.deposit(200)
-
-try:
-    a.deposit(-200)
-except ValueError as ve:
-    print(ve)
-
-a.authenticate()
-
-a.withdraw(10)
-a.balance
-try:
-    a.withdraw(-100)
-except ValueError as ve:
-    print(ve)
-
-try:   
-    a.withdraw(1000)
-except ValueError as ve:
-    print(ve)
-    
 # Part II : Minimum balance account
 
 class MinimumBalanceAccount(BankAccount):
@@ -72,12 +59,19 @@ class MinimumBalanceAccount(BankAccount):
         else:
             raise ValueError (f'Balance must be grater that {minmum_balance}')
 
-a = MinimumBalanceAccount('1','1',300,200)
+    def withdraw (self, amount):
+        if (self.balance - amount) >= self.minmum_balance:
+            super().withdraw(amount)   
+        else:
+            print (f'Balance after opertaion  must be grater that {self.minmum_balance}')
 
 # Part IV: BONUS Create an ATM class
 class ATM():
 
     def __init__(self, account_list, try_limit):
+        self.cooldown = 30
+        self.current_tries = 0
+        self.last_try_time = time.time()
         for acc in account_list:
             if isinstance(acc, BankAccount) or isinstance(acc, MinimumBalanceAccount):
                 self.account_list = account_list
@@ -92,29 +86,89 @@ class ATM():
                 print (ve)
             finally:
                 self.try_limit = 2
+            
+    def show_main_menu(self):
+        running = True
+        while running:
+            #os.system('cls')
+            print('Choose an option:')
+            choose = input('Press (l) to login, or (e) to exit:')
+            if choose == 'e':
+                running = False
+            elif choose == 'l':
+                if self.current_tries < self.try_limit:
+                    username = input('Enter username: ')
+                    password = input('Enter password: ')
+                    self.login(username, password)
+                elif time.time() - self.last_try_time > self.cooldown:
+                    self.current_tries -= 1
+                else:
+                    print(f'You enter wrong password more then {self.try_limit} times.')
+                    print(f'Input blocked for {self.cooldown} seconds.')
+                    print(f'{self.cooldown - (time.time() - self.last_try_time)} seconds remaining.')
+            else:
+                print('invalid option')
+    
+    def login(self, username, password):
+        for bank_account in self.account_list:            
+            if bank_account.authenticate_api(username, password):
+                self.show_account_menu(bank_account)
+                self.current_tries = 0
+                return None
+            else:
+                pass
+        print ('Wrong username or password')
+        self.current_tries += 1   
+        print (f'{self.try_limit - self.current_tries} attempt remaining')
 
-# Validates that try_limit is a positive number, if you get an invalid input raise an Exception, then move along and set try_limit to 2.
-# Hint: Check out this tutorial
+        self.last_try_time = time.time()  
+            
 
-# Sets attribute current_tries = 0
-
-# Call the method show_main_menu (see below)
-
-# Methods:
-# show_main_menu:
-# This method will start a while loop to display a menu letting a user select:
-# Log in : Will ask for the users username and password and call the log_in method with the username and password (see below).
-# Exit.
-
-# log_in:
-# Accepts a username and a password.
-
-# Checks the username and the password against all accounts in account_list.
-# If there is a match (ie. use the authenticate method), call the method show_account_menu.
-# If there is no match with any existing accounts, increment the current tries by 1. Continue asking the user for a username and a password, until the limit is reached (ie. try_limit attribute). Once reached display a message saying they reached max tries and shutdown the program.
-
-# show_account_menu:
-# Accepts an instance of BankAccount or MinimumBalanceAccount.
-# The method will start a loop giving the user the option to deposit, withdraw or exit.
+    def show_account_menu(self, bank_account):
+        running = True
+        while running:
+            print(f'You balance is {bank_account.balance}')
+            choice = input('Enter option: \n (w) - withdraw: \n (d) - deposit: \n (e) - exit: ')
+            if choice == 'w':
+                amount = int(input('Enter amount: '))
+                bank_account.withdraw(amount)
+            
+            elif choice == 'd':
+                amount = int(input('Enter amount: '))
+                bank_account.deposit(amount)
+            elif choice == 'e':
+                bank_account.authenticated = False
+                running = False
+            
 
 
+a = BankAccount( '1', '1', 100,)
+b = BankAccount( '2', '2', 200,)
+# print(a.balance)
+# a.deposit(200)
+
+# try:
+#     a.deposit(-200)
+# except ValueError as ve:
+#     print(ve)
+
+#a.authenticate()
+
+#a.withdraw(10)
+#a.balance
+# try:
+#     a.withdraw(-100)
+# except ValueError as ve:
+#     print(ve)
+
+# try:   
+#     a.withdraw(1000)
+# except ValueError as ve:
+#     print(ve)
+    
+lim = MinimumBalanceAccount('3','3',300,200)
+
+lis_acc = [a,b,lim]
+
+atm = ATM(lis_acc, 2)
+atm.show_main_menu()
