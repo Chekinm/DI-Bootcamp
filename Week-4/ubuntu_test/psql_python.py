0ea7758314f6ed68838432c386cb9f8798426eca
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-04-22 13:12:46 +0300

    ubunto_to_win_connect_test

---
+++ Week-4/ubuntu_test/psql_python.py
@@ -0,0 +1,47 @@
+import psycopg2
+
+HOSTNAME = '192.168.192.1'
+USERNAME = 'psyco_user'
+PASSWORD = 'psyco_user_password'
+DATABASE = 'psyco_test'
+
+try:
+    connection = psycopg2.connect(host = HOSTNAME, user = USERNAME, password = PASSWORD, dbname = DATABASE)
+except psycopg2.OperationalError as er:
+     connection = None
+     print (er)
+
+def run_select_query (connection, query):
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        a = cursor.fetchone()
+        yield a
+     
+
+def run_change_query (connection, query):
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        connection.commit()
+    return result
+
+def select_columns (table_name, *args):
+    columns = ','.join(args) if args else '*'
+    return  f'select {columns} from {table_name}'
+
+
+query = select_columns('test', 'user_name', 'user_age')
+if connection:
+    a_gen = run_select_query(connection, query)
+    
+    b = next(a_gen)
+    print(b)
+
+    # print(next(a_gen))
+    # with connection.cursor() as cursor:
+    #     cursor.execute(query)
+    #     print(type(cursor.fetchone()))
+    #     #print(a)
+    #     # a = cursor.fetchone()
+    #     # print(a)
+else:
+    raise ConnectionError ('we have a problem with connection to database')
