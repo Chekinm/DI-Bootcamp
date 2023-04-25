c1bbc1604576e6941ab8b2e9f737cf197a873cb8
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-04-25 15:29:41 +0300

    day-2 some fix

--- Week-5/Day-2/phone_number/phone_project_root/phone_project/settings.py
+++ Week-5/Day-2/phone_number/phone_project_root/phone_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'phone_app.apps.PhoneAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
