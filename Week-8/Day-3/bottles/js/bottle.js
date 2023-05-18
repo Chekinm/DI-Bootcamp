914377727ee4256b6ed3de8125fae63c9e96a7e1
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-05-18 14:26:07 +0300

    daily chalange

---
+++ Week-8/Day-3/bottles/js/bottle.js
@@ -0,0 +1,56 @@
+function isIntOneTo99(value) {
+    return (value) && /^[0-9]+$/.test(value) && value < 100 && value > 0;
+}
+
+function getUserInput(text, checkFunction) {
+    let input
+    do {
+        input = prompt (text);
+        flag = checkFunction(input);
+        if (!flag) alert('Wrong input') ;
+    } while (!flag);
+    console.log(input)
+    return input;
+}
+
+function setText(numbOfButtle,toDrink) {
+
+    if (toDrink >= numbOfButtle){
+        suffix = (numbOfButtle == 1) ? 's':''
+        plural = (numbOfButtle == 1) ?  'them' : 'it'
+        text = `Last ${numbOfButtle} buttle${suffix} remain one the wall. We pass all around, and no more at all. all--all--all! all--all--all! Now we drunk all--all--all!`
+        return {text:text,flag:false}
+    } else {
+        suffix = (toDrink == 1) ? 's':''
+        plural = (toDrink == 1) ? 'it' : 'them'
+        text = `Last ${numbOfButtle} buttle${suffix} remain one the wall. We pass ${toDrink} around. Now ${numbOfButtle-toDrink} bottles of beer, on, the wall.`
+        return {text:text,flag:true}
+    }   
+}
+
+function sayText(text) {
+    var u = new SpeechSynthesisUtterance();
+    u.text = text;
+    u.lang = 'en-US';
+    u.rate = 1.2;
+    //u.onend = function(event) { alert('Finished in ' + event.elapsedTime + ' seconds.'); }
+    speechSynthesis.speak(u);
+}
+
+
+function singASong() {
+
+    let numbOfButtle = getUserInput ("How many buttle we have? ", isIntOneTo99)
+    toDrink = 1
+    let response
+    do  {
+        response = setText(numbOfButtle, toDrink)
+        console.log(response.text)
+        sayText (response.text)
+        numbOfButtle = numbOfButtle - toDrink
+        toDrink++
+    } while (response.flag)
+}
+    
+
+
