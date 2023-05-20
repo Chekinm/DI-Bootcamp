3aa9eba557455aa3fd9e0963efb4b06f9793ec4a
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-05-21 02:03:59 +0300

    calcultaror

--- Week-8/Day-3/calculator/js/calculator.js
+++ Week-8/Day-3/calculator/js/calculator.js
@@ -1,21 +1,106 @@
 let result = document.getElementById("result")
-console.log(result.textContent)
-function number(value) {
 
-    result.textContent = value
+let bracketStack = []
 
+function number(value) {  
+
+    if (!result.textContent) {
+        result.textContent = value
+    } else if (!result.textContent.includes("Error")){
+        result.textContent += value
+    }
 }
 
 
-function operator () {
+function operator (operation) {
+    regex = /\d+|\)+$/
+    if (regex.test(result.textContent)) {
+        
+    
+    switch (operation) {
 
+        case ('+'):
+            result.textContent += '+'
+            break; 
+        
+        case ('-'):
+            result.textContent += '-'
+            break; 
+            
+        case ('*'):
+            result.textContent += '*'
+            break; 
+      
+        case ('/'):
+            result.textContent += '/'
+            break; 
 
+        case ('.'):
+            regex = /\d+$/
+            if (regex.test(result.textContent)) {
+                result.textContent += '.'}
+            break; 
+        }
+    }
+} 
 
+function brackets() {
+    console.log(bracketStack)
+    regexOpenBracket = /[(]+$|[\+\-\/\*]$/
+    regexCloseBracket = /\d+$|[)]+$/
+    if (!result.textContent){ 
+        console.log("Open bracket")
+        result.textContent = '('
+        bracketStack.push('(')
+    } else if (regexCloseBracket.test(result.textContent) && bracketStack.length) {
+        result.textContent += ')'
+        console.log("close bracket")
+        bracketStack.pop()
+    } else if (regexOpenBracket.test(result.textContent)) {
+        result.textContent += '('
+        console.log("open bracket")
+        bracketStack.push('(')
+
+    }
 }
 
-function equal () {
+function equal() {
+    while (bracketStack.length) {
+        result.textContent += ")"
+        bracketStack.pop()
+    } 
 
-
-
+    res = eval(result.textContent)
+    if (res==Infinity) {
+        alert("MathError(result)")
+        result.textContent = "Error. Press C"
+    } else {
+        result.textContent = res
+    }
 }
 
+
+// function isValid(value) {
+//     res = /^[\d()]+(?:[+*\/\./-][\d()]+)+$/.test(value)
+//     console.log(res)
+//     return res
+// }
+
+function c () {
+    result.textContent = ''
+    bracketStack = []
+}
+
+function bs() {
+    if (result.textContent[result.textContent.length-1]==='(') {
+
+        bracketStack.pop()
+        console.log(bracketStack)
+    } else if (result.textContent[result.textContent.length-1]===')') {
+
+        bracketStack.push('(')
+        console.log(bracketStack)
+    } 
+    result.textContent = result.textContent.slice(0,result.textContent.length-1)
+    console.log(result.textContent)
+}
