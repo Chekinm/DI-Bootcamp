278256c603b423bfe115d74606a0cedab9aa22ca
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-05-23 17:09:59 +0300

    xp ex

--- Week-9/Day-3/XP/xp3.js
+++ Week-9/Day-3/XP/xp3.js
@@ -2,5 +2,6 @@ const epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
 
 let realEpicString = "May the force be with you!" //just kidding
 
-let epicString = epic.reduce((total, word)=>total+=word+' ').trim()
+let epicString = epic.reduce((total, word) => total += word + ' ').trim()
 console.log(epicString)
+
