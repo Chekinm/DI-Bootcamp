a6f0e52a4640b10136f05fefdb17eacbe35106d5
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-05-29 10:44:01 +0300

    add one stringer

--- Week-9-1/Day-1/dailychallange/dcgold.js
+++ Week-9-1/Day-1/dailychallange/dcgold.js
@@ -12,4 +12,14 @@ console.log(allTruthy(true, true, true))
 
 console.log(allTruthy(true, false, true))
 
-console.log(allTruthy(5, 4, 3, 2, 1, ))
+console.log(allTruthy(5, 4, 3, 2, 1, 0))
+
+//or, if you prefer one string solutions
+
+const allTruthyOneStringer=(...args) => Boolean(args.reduce((answer, currElem)=>answer && currElem))
+
+console.log(allTruthyOneStringer(true, true, true))
+
+console.log(allTruthyOneStringer(true, false, true))
+
+console.log(allTruthyOneStringer(5, 4, 3, 2, 1, NaN ))
