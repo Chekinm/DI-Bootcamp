8d5b8d340e93c49c9310a724a098cd91b9159998
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-06-18 09:33:17 +0300

    FIx bug with deleting order

--- Week-11/Day-5/todo-list/src/components/ToDoList.js
+++ Week-11/Day-5/todo-list/src/components/ToDoList.js
@@ -8,8 +8,10 @@ const ToDoList = (props) => {
 
     const deleteToDo = (e) => {
         // create new arr with deleted clicked element
+        
         let updatedArr = props.props.toDoList.toSpliced(e.target.getAttribute("name"),1)
         // send updated array to parent to rerender tree
+        
         props.props.callBack(updatedArr)
     }
     return (
@@ -19,7 +21,7 @@ const ToDoList = (props) => {
                         key={id} 
                         onClick={deleteToDo}  
                         name={id}>
-                        <span strike hover-s>{item}</span>
+                        {item}
                     </li>
             }
             )
